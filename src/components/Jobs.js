8e5b828aaae584698a3f08b2f2110000c01d929d
Cathy Dao <cathydao95@gmail.com>
2022-07-11 20:31:42 -0700

    map through data to display

--- src/components/Jobs.js
+++ src/components/Jobs.js
@@ -1,13 +1,17 @@
-function Jobs() {
-  return (
-    <div>
+function Jobs({ dataArray }) {
+  console.log(dataArray);
+
+  const items = dataArray.map((item, index) => {
+    return (
       <div>
-        <h2>Company Name</h2>
-        <p>Is hiring...</p>
-        <span>Date</span>
+        <h2>{dataArray[index].title.substring(0, indexOf("is hiring"))}</h2>
+        <p>{dataArray[index].title}</p>
+        <span>Date {dataArray[index].time}</span>
       </div>
-    </div>
-  );
+    );
+  });
+
+  return <div>{items}</div>;
 }
 
 export default Jobs;
