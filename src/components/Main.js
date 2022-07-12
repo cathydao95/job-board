173fc657f2399fadb04a3e211181b066825a7ec7
Cathy Dao <cathydao95@gmail.com>
2022-07-12 15:33:25 -0700

    function to load 6 more posts

--- src/components/Main.js
+++ src/components/Main.js
@@ -13,16 +13,31 @@ function Main() {
       .then((data) => setData(data));
   }, []);
 
+  //   useEffect(() => {
+  //     const testSlice = data.slice(0, load);
+  //     console.log(testSlice);
+  //     const newLoad = []
+  //     testSlice.map((item) => {
+  //       fetch(`https://hacker-news.firebaseio.com/v0/item/${item}.json`)
+  //         .then((res) => res.json())
+  //         .then((data) => setDataArray((prevData) => [...prevData, data]));
+  //     });
+  //   }, [data, load]);
+
+  // NEED TO FIGURE OUT HOW TO DISPLAY
   useEffect(() => {
     const testSlice = data.slice(0, load);
     console.log(testSlice);
+    const newLoad = [];
     testSlice.map((item) => {
       fetch(`https://hacker-news.firebaseio.com/v0/item/${item}.json`)
         .then((res) => res.json())
-        .then((data) => setDataArray((prevData) => [...prevData, data]));
+        .then((data) => newLoad.push(data));
     });
+    setDataArray(newLoad);
   }, [data, load]);
 
+  // load 6 more posts
   function loadMore() {
     setLoad((prevLoad) => prevLoad + 6);
   }
@@ -30,11 +45,12 @@ function Main() {
   return (
     <div>
       <h1>HN Jobs</h1>
-      {/* {dataArray.length > 0 ? <Jobs dataArray={dataArray} /> : null} */}
       <div className="job-container">
-        {dataArray.length > 0
-          ? dataArray.map((item) => <Jobs item={item} />)
-          : null}
+        {dataArray.length > 0 ? (
+          dataArray.map((item) => <Jobs item={item} />)
+        ) : (
+          <div>"Loading"</div>
+        )}
       </div>
 
       <button onClick={loadMore}>Load More</button>
