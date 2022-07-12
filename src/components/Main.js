f6fe502cb21e22ef17d1b7ec01477f895b112818
Cathy Dao <cathydao95@gmail.com>
2022-07-11 20:31:34 -0700

    fetch data from api

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,38 @@
 import Jobs from "./Jobs";
 import { useState, useEffect } from "react";
+
 function Main() {
+  const [data, setData] = useState([]);
+  const [dataArray, setDataArray] = useState([]);
+
+  useEffect(() => {
+    fetch("https://hacker-news.firebaseio.com/v0/jobstories.json")
+      .then((res) => res.json())
+      .then((data) => setData(data));
+  }, []);
+
+  useEffect(() => {
+    const testSlice = data.slice(0, 9);
+    testSlice.map((item) => {
+      fetch(`https://hacker-news.firebaseio.com/v0/item/${item}.json`)
+        .then((res) => res.json())
+        .then((data) => setDataArray((prevData) => [...prevData, data]));
+    });
+  }, [data]);
+
+  //   const dataArray = data.map((item) => {
+  //     fetch(`https://hacker-news.firebaseio.com/v0/item/${item}.json`)
+  //       .then((res) => res.json())
+  //       .then((data) => console.log(data));
+  //   });
+
+  console.log(data);
+
   return (
     <div>
       <h1>HN Jobs</h1>
-      <Jobs />
+      {dataArray.length > 0 ? <Jobs dataArray={dataArray} /> : null}
+
       <button>Load More</button>
     </div>
   );
